a59e3a62ced5007b6c8edaa4f28ddbb5e1aa7b33
Henry Johnson <jan.hanyu@outlook.com>
2021-10-02 22:40:17 +0800

    update blog config

--- blog.config.js
+++ blog.config.js
@@ -4,34 +4,40 @@ const BLOG = {
   email: 'i@crabass.me',
   link: 'https://crabass.vercel.app',
   description: 'Welcome to Henry\'s blog!',
-  lang: 'en-US',
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
+  font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#111827', // use hex value, don't forget '#'
+  darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2021, // if leave this empty, current year will be used.
-  postsPerPage: 10,
-  showAbout: true, // WIP
-  showArchive: true, // WIP
+  since: 2021, // If leave this empty, current year will be used.
+  postsPerPage: 7,
+  sortByDate: false,
+  showAbout: true,
+  showArchive: true,
+  autoCollapsedNavBar: false, // The automatically collapsed navigation bar
+  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://github.com/CrabAss',
   seo: {
     keywords: ['Blog', 'Website', 'Henry Johnson', 'tech', 'CrabAss'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
+  notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    provider: 'ackee', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
-      tracker: 'https://ackee.craigary.net/tracker.js', // e.g 'https://ackee.craigary.net/tracker.js'
-      dataAckeeServer: 'https://ackee.craigary.net/', // e.g https://ackee.craigary.net , don't end with a slash
-      domainId: '0e2257a8-54d4-4847-91a1-0311ea48cc7b' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+      tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
+      dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
+      domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: 'G-PPKMNRCKJK' // e.g: G-XXXXXXXXXX
+      measurementId: 'G-1MQV3C42X3' // e.g: G-XXXXXXXXXX
     }
   },
-  comment: { // support provider: gitalk
-    provider: '', // leave it empty if you don't need any comment plugin
+  comment: {
+    // support provider: gitalk, utterances, cusdis
+    provider: 'utterances', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
       repo: '', // The repository of store comments
       owner: '',
@@ -39,8 +45,17 @@ const BLOG = {
       clientID: '',
       clientSecret: '',
       distractionFreeMode: false
+    },
+    utterancesConfig: {
+      repo: 'CrabAss/nobelium'
+    },
+    cusdisConfig: {
+      appId: '', // data-app-id
+      host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
+      scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
-  }
+  },
+  isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
 }
 // export default BLOG
 module.exports = BLOG
