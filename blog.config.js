81b992fbee5096c9e443e6ff381f05210f2d2236
CrabAss <i@crabass.me>
2021-10-04 02:44:51 +0800

    config update

--- blog.config.js
+++ blog.config.js
@@ -2,8 +2,8 @@ const BLOG = {
   title: 'CrabLog',
   author: 'Henry Johnson',
   email: 'i@crabass.me',
-  link: 'https://crabass.vercel.app',
-  description: 'Welcome to Henry\'s blog!',
+  link: 'https://crabass.me',
+  description: 'welcome to Henry\'s blog!',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
@@ -11,7 +11,7 @@ const BLOG = {
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
-  postsPerPage: 7,
+  postsPerPage: 10,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
